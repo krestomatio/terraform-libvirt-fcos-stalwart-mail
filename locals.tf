85133e6275bd69bc1f9ed50b57172866c2bbbdc3
Job Céspedes Ortiz <jobcespedes@gmail.com>
2023-08-17 23:13:22 -0600

    refactor: standarize hook script

--- locals.tf
+++ locals.tf
@@ -2,39 +2,42 @@ locals {
   data_volume_path     = "/var/opt/stalwart-mail"
   systemd_stop_timeout = 30
   stalwart_mail_image  = "${var.image.name}:${var.image.version}"
+  ssl_path             = "/etc/certs/${var.external_fqdn}"
   post_hook = {
     path    = "/usr/local/bin/stalwart-mail-certbot-renew-hook"
     content = <<-TEMPLATE
       #!/bin/bash
 
       # vars
-      stalwart_mail_cert_folder_path="${local.data_volume_path}/etc/certs/${var.external_fqdn}"
-      stalwart_mail_cert_path="$$${stalwart_mail_cert_folder_path}/fullchain.pem"
-      stalwart_mail_key_path="$$${stalwart_mail_cert_folder_path}/privkey.pem"
-      stalwart_mail_proxy_uid="0"
-      stalwart_mail_proxy_gid="0"
+      container_name=stalwart-mail
+      cert_folder_path="${local.data_volume_path}/${local.ssl_path}"
+      cert_path="$$${cert_folder_path}/fullchain.pem"
+      key_path="$$${cert_folder_path}/privkey.pem"
+      key_path="0"
+      proxy_gid="0"
       source_cert_folder_path="/etc/letsencrypt/live/${var.external_fqdn}"
       source_cert_path="$$${source_cert_folder_path}/fullchain.pem"
       source_key_path="$$${source_cert_folder_path}/privkey.pem"
 
       # handle cert correct placement
       # dir
-      mkdir -p $$${stalwart_mail_cert_folder_path}
+      mkdir -p $$${cert_folder_path}
       # cert
-      cp -f "$$${source_cert_path}" "$$${stalwart_mail_cert_path}"
+      cp -f "$$${source_cert_path}" "$$${cert_path}"
       # key
-      cp -f "$$${source_key_path}" "$$${stalwart_mail_key_path}"
+      cp -f "$$${source_key_path}" "$$${key_path}"
       # owner
-      chown $$${stalwart_mail_proxy_uid}:$$${stalwart_mail_proxy_gid} "$$${stalwart_mail_cert_folder_path}" "$$${stalwart_mail_cert_path}" "$$${stalwart_mail_key_path}"
+      chown $$${key_path}:$$${proxy_gid} "$$${cert_folder_path}" "$$${cert_path}" "$$${key_path}"
       # permissions
-      chmod 0600 "$$${stalwart_mail_cert_path}" "$$${stalwart_mail_key_path}"
+      chmod 0600 "$$${cert_path}" "$$${key_path}"
 
       # restart container
-      if podman ps stalwart-mail &> /dev/null
+      if podman ps --format "{{.Names}}" 2> /dev/null | grep -q -w $container_name
       then
-        podman restart stalwart-mail
+        echo "restarting $container_name container..."
+        podman restart $container_name
       else
-        echo "stalwart-mail container not running"
+        echo "$container_name container not running"
       fi
     TEMPLATE
   }
